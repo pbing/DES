c6a0da255323b9339805508e3a4cf75009add74e
Bernd Beuster <bernd.beuster@googlemail.com>
2011-11-06 17:10:11 +0100

    Added CBC mode.

--- demo/benchmark.lisp
+++ demo/benchmark.lisp
@@ -1,14 +1,32 @@
 ;;; Benchmarks
 
-(defparameter *block-length* 1000000)
+(defparameter *block-length* 10000000)
 (defparameter *key* (random (expt 2 64)))
+(defparameter *iv* (random (expt 2 64)))
 
-(defparameter *plain-text*  (make-array *block-length* :element-type '(unsigned-byte 64)))
-(defparameter *cipher-text* (make-array *block-length* :element-type '(unsigned-byte 64)))
+(defparameter *plain-text*   (make-array *block-length* :element-type '(unsigned-byte 64)))
+(defparameter *cipher-text*  (make-array *block-length* :element-type '(unsigned-byte 64)))
+(defparameter *scratch-text* (make-array *block-length* :element-type '(unsigned-byte 64)))
 
+(defun init-text (text)
+  (loop for i below (length text) do
+    (setf (aref text i) i)))
+
+(defun clear-text (text)
+  (loop for i below (length text) do
+    (setf (aref text i) 0)))
+
+(defun print-text (text &optional (n 10))
+  (setf n (or n (length text)))
+  (loop for i below n do
+    (format t "~&~16,'0X~%" (aref text i))))
 
 (defun benchmark-encrypt-ecb ()
   (des:block-encrypt-ecb *plain-text* *cipher-text* *key*)
+  (values))
+
+(defun benchmark-encrypt-cbc ()
+  (des:block-encrypt-cbc *plain-text* *cipher-text* *key* :iv *iv*)
   (values))
 
 #+(or)
